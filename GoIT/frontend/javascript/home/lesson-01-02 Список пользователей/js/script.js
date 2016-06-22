a6da9e889c2706c072b00e79f3b174ab30573f8b
Evgeni Markov <mp_savage@list.ru>
2016-06-22 23:39:07 +0300

    java task 2 array names empty field

--- GoIT/frontend/javascript/home/lesson-01-02 Список пользователей/js/script.js
+++ GoIT/frontend/javascript/home/lesson-01-02 Список пользователей/js/script.js
@@ -11,12 +11,18 @@ var arrayLenght = arr.length;
 console.log(arrayLenght);
 var user = prompt('Введите имя Пользователя');
 var t = 0;
-while (t < arrayLenght) {
-    if (user === arr[t]) {
+console.log(user);
+if (user === '') {
+    alert('Ошибка. Введенное имя пользователя не существует в массиве');
+    t = 6;
+} else {
+    while (t < arrayLenght) {
+        if (user === arr[t]) {
         alert(user +', Вы успешно вошли');
         t = 6;
     } else {
         t++;
+    }
     }
 }
 if (t != 6) {
