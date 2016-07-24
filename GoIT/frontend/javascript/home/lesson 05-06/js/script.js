f715035d84995e58453853ba7fafc43c16e1855f
Evgeni Markov <mp_savage@list.ru>
2016-07-25 00:16:31 +0300

    homework 06 bonus task

--- GoIT/frontend/javascript/home/lesson 05-06/js/script.js
+++ GoIT/frontend/javascript/home/lesson 05-06/js/script.js
@@ -18,6 +18,7 @@ var dot = document.createElement('div');
 dot.classList.add('dot', 'inline');
 dot.innerHTML = '.';
 var globalTime = 0;
+var globalTime2 = 0;
 var h = Math.floor(globalTime / 3600000) % 24;
 var m = Math.floor(globalTime / 60000) % 60;
 var s = Math.floor(globalTime / 1000) % 60;
@@ -35,7 +36,74 @@ timerDisplay.insertBefore(dots2, null);
 timerDisplay.insertBefore(seconds, null);
 timerDisplay.insertBefore(dot, null);
 timerDisplay.insertBefore(miliseconds, null);
+var start = document.createElement('button');
+start.value = 'start';
+start.innerHTML = 'START';
+start.classList.add('btn-success');
+body.appendChild(start);
+var stop = document.createElement('button');
+stop.value = 'stop';
+stop.innerHTML = 'CLEAR';
+stop.classList.add('btn-danger');
+body.appendChild(stop);
+var pause = 'paused';
+z = 0;
+
+var timerDisplay2 = document.createElement('div');
+timerDisplay2.classList.add('display');
+var hours2 = document.createElement('div');
+hours2.classList.add('hours', 'inline');
+var minutes2 = document.createElement('div');
+minutes2.classList.add('minutes', 'inline');
+var seconds2 = document.createElement('div');
+seconds2.classList.add('seconds', 'inline');
+var miliseconds2 = document.createElement('div');
+miliseconds2.classList.add('miliseconds', 'inline');
+var h2 = Math.floor(globalTime2 / 3600000) % 24;
+var m2 = Math.floor(globalTime2 / 60000) % 60;
+var s2 = Math.floor(globalTime2 / 1000) % 60;
+var ms2 = Math.floor(globalTime2 % 1000);
+hours2.innerHTML = h2;
+minutes2.innerHTML = m2;
+seconds2.innerHTML = s2;
+miliseconds2.innerHTML = ms2;
+var dots3 = document.createElement('div');
+dots3.innerHTML = ':';
+dots3.classList.add('dots', 'inline');
+var dots4 = document.createElement('div');
+dots4.innerHTML = ':';
+dots4.classList.add('dots', 'inline');
+var dot2 = document.createElement('div');
+dot2.classList.add('dot', 'inline');
+dot2.innerHTML = '.';
+body.appendChild(timerDisplay2);
+timerDisplay2.insertBefore(hours2, null);
+timerDisplay2.insertBefore(dots3, null);
+timerDisplay2.insertBefore(minutes2, null);
+timerDisplay2.insertBefore(dots4, null);
+timerDisplay2.insertBefore(seconds2, null);
+timerDisplay2.insertBefore(dot2, null);
+timerDisplay2.insertBefore(miliseconds2, null);
+var start2 = document.createElement('button');
+start2.value = 'start2';
+start2.innerHTML = 'START';
+start2.classList.add('btn-success');
+body.appendChild(start2);
+var split = document.createElement('button');
+split.value = 'split';
+split.innerHTML = 'SPLIT';
+split.classList.add('btn-warning');
+body.appendChild(split);
+var pause2 = 'paused';
+var reset = document.createElement('button');
+reset.value = 'reset';
+reset.innerHTML = 'RESET';
+reset.classList.add('btn-danger');
+body.appendChild(reset);
+var ms3 = "000";
+
 var inline = document.querySelectorAll('.inline');
+console.log(inline);
 inline[0].style.display = 'inline-block';
 inline[0].style.fontSize = '50px';
 inline[1].style.display = 'inline-block';
@@ -50,17 +118,24 @@ inline[5].style.display = 'inline-block';
 inline[5].style.fontSize = '50px';
 inline[6].style.display = 'inline-block';
 inline[6].style.fontSize = '50px';
-var start = document.createElement('button');
-start.value = 'start';
-start.innerHTML = 'START';
-start.classList.add('btn-success');
-body.appendChild(start);
-var stop = document.createElement('button');
-stop.value = 'stop';
-stop.innerHTML = 'CLEAR';
-stop.classList.add('btn-danger');
-body.appendChild(stop);
-var pause = 'paused';
+inline[7].style.display = 'inline-block';
+inline[7].style.fontSize = '50px';
+inline[8].style.display = 'inline-block';
+inline[8].style.fontSize = '50px';
+inline[9].style.display = 'inline-block';
+inline[9].style.fontSize = '50px';
+inline[10].style.display = 'inline-block';
+inline[10].style.fontSize = '50px';
+inline[11].style.display = 'inline-block';
+inline[11].style.fontSize = '50px';
+inline[12].style.display = 'inline-block';
+inline[12].style.fontSize = '50px';
+inline[13].style.display = 'inline-block';
+inline[13].style.fontSize = '50px';
+
+
+
+
 function timerStart() {
     pause = 'flow';
     start.innerHTML = 'PAUSE';
@@ -114,5 +189,103 @@ function clearAll() {
         console.log(ms);
     }
 }
+
+var x=0;
+
 start.addEventListener('click', timerStart);
 stop.addEventListener('click', clearAll);
+start2.addEventListener('click', timerStart2);
+reset.addEventListener('click', clearAll2);
+split.addEventListener('click', split2);
+
+
+function timerStart2() {
+    pause2 = 'flow';
+    start2.innerHTML = 'STOP';
+    start2.removeEventListener('click', timerStart2);
+    start2.addEventListener('click', timerPause2);
+    var timerId = setInterval(time, 4);
+    z = 0;
+    function time() {
+        globalTime2 = globalTime2 + 4;
+        h2 = Math.floor(globalTime2 / 3600000) % 24;
+        m2 = Math.floor(globalTime2 / 60000) % 60;
+        s2 = Math.floor(globalTime2 / 1000) % 60;
+        ms2 = Math.floor(globalTime2 % 1000);
+        if (ms2 < 10) {
+            ms2 = "0" + "0" + ms2;
+        } else if (ms2 < 100) {
+            ms2 = "0" + ms2;
+        }
+        hours2.innerHTML = h2;
+        minutes2.innerHTML = m2;
+        seconds2.innerHTML = s2;
+        miliseconds2.innerHTML = ms2;
+        if (pause2 === 'paused') {
+            clearInterval(timerId);
+        }
+    }
+}
+function timerPause2() {
+    'use strict';
+    x=x+1;
+    pause2 = 'paused';
+    start2.innerHTML = 'START';
+    start2.removeEventListener('click', timerPause2);
+    start2.addEventListener('click', timerStart2);
+    var check = document.createElement('div');
+    check.classList.add('check');
+    check.classList.add('check' + "_" + x);
+    var ms3 = ms2 + 4;
+    check.innerHTML = (x + "." + "STOP" + " " + h2 + ":" + m2 + ":" + s2 + "." + ms3);
+    body.appendChild(check);
+    console.log(check);
+}
+function split2() {
+    x = x + 1;
+    'use strict';
+    if (ms2 === 0) {
+        ms3 = "000"
+    } else if (ms2 < 10) {
+        ms3 = "0" + "0" + ms2;
+        } else if (ms2 < 100) {
+            ms3 = "0" + ms2;
+        } else {
+            ms3 = ms2
+        }
+    var check = document.createElement('div');
+    console.log(x);
+    console.log(s2);
+    console.log("SPLIT");
+    check.classList.add('check');
+    check.classList.add('check' + "_" + x);
+    check.innerHTML = (x + "." + "SPLIT" + " " + h2 + ":" + m2 + ":" + s2 + "." + ms3);
+    body.appendChild(check);
+    console.log(check);    
+}
+function clearAll2() {
+    start2.removeEventListener('click', timerPause2);
+    start2.addEventListener('click', timerStart2);
+    start2.innerHTML = 'START';
+    pause2 = 'paused';
+    setTimeout(clear, 6);
+    function clear() {
+        globalTime2 = 0;
+        h2 = Math.floor(globalTime2 / 3600000) % 24;
+        m2 = Math.floor(globalTime2 / 60000) % 60;
+        s2 = Math.floor(globalTime2 / 1000) % 60;
+        ms2 = Math.floor(globalTime2 % 1000);
+        hours2.innerHTML = h2;
+        minutes2.innerHTML = m2;
+        seconds2.innerHTML = s2;
+        miliseconds2.innerHTML = ms2;
+        var check2 = document.querySelectorAll('.check');
+        console.log(check2);
+        console.log(check2.length);
+        for (z=0; z<check2.length; z++) {
+            check2[z].remove();
+        }
+        x = 0;
+    }
+}
+console.log(split);
