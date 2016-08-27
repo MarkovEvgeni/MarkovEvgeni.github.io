c2ee14bc711403f69a386f7f01ed3338ebe3b74d
Evgeni Markov <mp_savage@list.ru>
2016-08-27 11:50:36 +0300

    lesson 20. lodash

---
+++ GoIT/frontend/javascript/home/lesson 20 JSON LoDash/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    
+    console.log('Привет');
+    
+    var data = $.ajax({
+        url: 'js/data.json',
+        dataType: "json"
+    });
+    
+    console.log(data);
+    
+
+});
