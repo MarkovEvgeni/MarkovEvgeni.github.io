0ee9cf37dbff5a5ce06c36535728aa426114726d
Evgeni Markov <mp_savage@list.ru>
2016-08-27 21:05:43 +0300

    lesson 20. lodash

--- GoIT/frontend/javascript/home/lesson 20/js/script.js
+++ GoIT/frontend/javascript/home/lesson 20/js/script.js
@@ -1,4 +1,6 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    
     var $headersection = $('.main_menu li a');
     $headersection.on("click", function (e) {
         e.preventDefault();
@@ -25,5 +27,45 @@ $(function () {
             $(this).prev('.plusminus').addClass("yellowback");
             $(this).prev('.plusminus').addClass("minus");
         }
-    });  
+    });
+    
+    var data = $.ajax({
+        url: 'js/data.json',
+        dataType: "json"
+    });
+    
+    var testing = data.responseJSON;
+    
+    // 1. Массив скиллов (поле skills) всех людей, не должно быть повторяющихся скиллов, так же они должны быть отсортированы по алфавиту;
+    var skills = _.map(testing, 'skills');
+    var skills2 =_.flatten(skills);
+    skills = _.uniq(skills2).sort(function (a,b) {
+        return a.localeCompare(b, 'en', {'sensitivity': 'base'});
+    });
+    console.log(skills);
+    
+// 2. Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (friends) - ПО УБЫВАНИЮ;    
+    var name = _.map(testing, 'name');
+    var friends = _.map(testing, 'friends');
+    friends = _.mapValues(friends, function (o) {
+        return o.length;
+    });
+    friends = _.toArray(friends);
+    var full = _.zip(name, friends).sort(function (a, b) {
+        if (a[1] > b[1]) {return -1; }
+             else {return 1;}
+    });
+    full = _.map(full, 0);
+    console.log(full);
+    
+//3. Массив всех друзей всех пользователей, не должно быть повторяющихся людей
+    
+    var buddys =_.map(testing, 'friends');
+    buddys =_.flattenDeep(buddys);
+    buddys = _.map(buddys, 'name');
+    buddys = _.uniq(buddys);
+    console.log(buddys);
+    
+    
 });
+
