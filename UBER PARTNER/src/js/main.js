9399a61c2e917593634eaeb728a20317744ee1d7
Evgeni Markov <mp_savage@list.ru>
2017-02-11 19:40:56 +0200

    Initial UBER commit

---
+++ UBER PARTNER/src/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    'use strict';
+    
+    //
+    //Third party components
+    //
+
+    //
+    //Custom components
+    //
+
+    //
+    //Custom components typescript
+    //
+
+    //= ts/tscript.js
+
+});
