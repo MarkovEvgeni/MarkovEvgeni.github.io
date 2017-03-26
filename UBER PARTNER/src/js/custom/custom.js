4506941bff8ea40df4fba8edb19367c728f0ffa1
Evgeni Markov <mp_savage@list.ru>
2017-03-26 22:04:44 +0300

    small fixes with our advantages

--- UBER PARTNER/src/js/custom/custom.js
+++ UBER PARTNER/src/js/custom/custom.js
@@ -28,4 +28,24 @@ $(".maxified ul li").on("click","a", function (event) {
     
         $('body,html').animate({scrollTop: top}, 1500);
     
-    });
+});
+
+$(".promo .button_container .button").on("click","a", function (event) {
+    
+        //отменяем стандартную обработку нажатия по ссылке
+    
+        event.preventDefault();
+    
+        //забираем идентификатор бока с атрибута href
+    
+        var id  = $(this).attr('href');
+            
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+    
+        var top = $(id).offset().top - 48;
+    
+        //анимируем переход на расстояние - top за 1500 мс
+    
+        $('body,html').animate({scrollTop: top}, 1500);
+    
+});
