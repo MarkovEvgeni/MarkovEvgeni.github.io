bb110acdf3b7937cfbed97ce1bb5a7da029e6379
Evgeni Markov <mp_savage@list.ru>
2017-03-29 00:35:51 +0300

    slider and terms updates

--- UBER PARTNER/src/js/custom/custom.js
+++ UBER PARTNER/src/js/custom/custom.js
@@ -1,12 +1,10 @@
-console.log('hello!');
-
 $('.slider_container').slick({
     speed: 300,
     slidesToShow: 1,
     adaptiveHeight: true,
     autoplay: true,
-    prevArrow: '<div class="slick-prev">&lsaquo;</div>',
-    nextArrow: '<div class="slick-next">&rsaquo;</div>'
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>'
 });
 
 
@@ -31,6 +29,26 @@ $(".maxified ul li").on("click","a", function (event) {
 });
 
 $(".promo .button_container .button").on("click","a", function (event) {
+    
+        //отменяем стандартную обработку нажатия по ссылке
+    
+        event.preventDefault();
+    
+        //забираем идентификатор бока с атрибута href
+    
+        var id  = $(this).attr('href');
+            
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+    
+        var top = $(id).offset().top - 48;
+    
+        //анимируем переход на расстояние - top за 1500 мс
+    
+        $('body,html').animate({scrollTop: top}, 1500);
+    
+});
+
+$(".slider_container .button_container .button").on("click","a", function (event) {
     
         //отменяем стандартную обработку нажатия по ссылке
     
