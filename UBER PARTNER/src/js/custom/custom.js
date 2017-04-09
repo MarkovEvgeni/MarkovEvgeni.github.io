2c9af31da6ba261171887515436cc0c21184035d
Evgeni Markov <evg.markov.fe@gmail.com>
2017-04-09 14:30:47 +0300

    updates

--- UBER PARTNER/src/js/custom/custom.js
+++ UBER PARTNER/src/js/custom/custom.js
@@ -67,3 +67,23 @@ $(".slider_container .button_container .button").on("click","a", function (event
         $('body,html').animate({scrollTop: top}, 1500);
     
 });
+
+$(".easy_container .button_container .button").on("click","a", function (event) {
+    
+        //отменяем стандартную обработку нажатия по ссылке
+    
+        event.preventDefault();
+    
+        //забираем идентификатор бока с атрибута href
+    
+        var id  = $(this).attr('href');
+            
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+    
+        var top = $(id).offset().top - 48;
+    
+        //анимируем переход на расстояние - top за 1500 мс
+    
+        $('body,html').animate({scrollTop: top}, 1500);
+    
+});
