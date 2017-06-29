0dc061d27d7bcca6c47bac57f08a4404e6474c69
EvgeniMarkovLanars <markov.e@lanars.com>
2017-06-29 11:58:15 +0300

    video6

--- video/js/main.js
+++ video/js/main.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded",function(){"use strict";console.log("test");var e=document.getElementsByTagName("video")[0];console.log(e),setTimeout(function(){e.play()},3e3)});
+document.addEventListener("DOMContentLoaded",function(){"use strict";console.log("test");var e=document.getElementsByTagName("video")[0];setTimeout(function(){e.play()},3e3)});
