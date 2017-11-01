12e1663eb626c67b09c7a135a092ab54011c1eeb
Markov Evgeni <evg.markov.fe@gmail.com>
2017-11-01 20:25:10 +0200

    landing update

--- Adam Landing/src/js/main.js
+++ Adam Landing/src/js/main.js
@@ -1,5 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-    'use strict';
-    
+  
+  'use strict';
+  
+  setTimeout(function() {
+    var glow = document.getElementsByClassName('first_screen__glow');
+    var mask = document.getElementsByClassName('first_screen__mask');
+    glow[0].classList.add('animation_started');
+    mask[0].classList.add('animation_started');
+    setTimeout(function() {
+      startScrolling();
+    }, 8500)
+  }, 50);
+  
+  function startScrolling () {
+    var firstScreen = document.getElementsByClassName('first_screen');
+    var secondScreen = document.getElementsByClassName('second_screen');
+    firstScreen[0].classList.add('animation_finished');
+    secondScreen[0].classList.add('animation_finished');
+    var scrollHeight = document.documentElement.clientHeight;
+    scrollScreen(scrollHeight);
+  }
+  
+  function scrollScreen(scrollHeightValue) {
+    var currentScroll = window.pageYOffset;
+    var scrollScreenInterval = setInterval(function () {
+      if (currentScroll + 4 < scrollHeightValue) {
+        window.scrollBy(0, 4);
+        currentScroll += 4;
+      } else {
+        clearInterval(scrollScreenInterval);
+        window.scrollBy({
+          left: 0,
+          top: document.documentElement.clientHeight,
+          behavior: 'smooth'
+        });
+      }
+    }, 4);
+    return;
+  } 
 });
