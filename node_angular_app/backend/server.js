5026093bd3fb6af8bc6ba5b8207eff90f57b6a1d
Markov Evgeni <evg.markov.fe@gmail.com>
2018-01-02 19:21:01 +0200

    angular learning

---
+++ node_angular_app/backend/server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var messages = [
+  {
+    text:'some text',
+    owner: 'Tim'
+  },
+  {
+    text:'other message',
+    owner: 'Mike'
+  }
+];
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+var api = express.Router();
+
+api.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
+api.post('/messages', (req, res) => {
+  messages.push(req.body);
+  res.json(req.body);
+});
+
+app.use('/api', api);
+
+app.listen(63145);
